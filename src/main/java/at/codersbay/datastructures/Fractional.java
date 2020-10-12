b739f5439f9f64c05fed04ad232f71fae12732a4
Jakob Penninger <jakob.penninger@gmx.net>
2020-10-12 14:32:35 +0200

    Add Main

--- src/main/java/at/codersbay/datastructures/Fractional.java
+++ src/main/java/at/codersbay/datastructures/Fractional.java
@@ -6,39 +6,58 @@ package at.codersbay.datastructures;
  */
 public class Fractional {
 
+    public Integer numerator;
+    public Integer denominator;
+
     public Fractional(Integer numerator, Integer denominator) {
         // TODO implement to fix tests in FractionalTest1
+        if (denominator == null || numerator == null) {
+            throw new IllegalArgumentException("You shall not pass null!");
+        } else if (denominator == 0) {
+            throw new IllegalArgumentException("You shall not divide through zero!");
+        } else {
+            this.denominator = denominator;
+            this.numerator = numerator;
+        }
     }
 
     public Integer getNumerator() {
         // TODO implement to fix tests in FractionalTest1
-        return -1;
+        return numerator;
     }
 
     public Integer getDenominator() {
         // TODO implement to fix tests in FractionalTest1
-        return -1;
+        return denominator;
     }
 
     public Float asFloat() {
         // TODO implement to fix tests in FractionalTest2
-        return -1f;
+        return (float) numerator / (float) denominator;
     }
 
     @Override
     public boolean equals(Object obj) {
         // TODO implement to fix tests in FractionalTest3
-        return false;
+        Fractional other = (Fractional) obj;
+        if (other.denominator == 0 || other.numerator == 0) return true;
+        else if (obj == null) return false;
+        else if (obj == this) return true;
+        else if (other.denominator == denominator && other.numerator == numerator) return true;
+        else return false;
     }
+
 
     public Fractional multiply(Fractional other) {
         // TODO implement to fix tests in FractionalTest4
-        return new Fractional(1, 1);
+        Fractional frac = other;
+        return new Fractional(numerator * frac.numerator, denominator * frac.denominator);
     }
 
     public Fractional multiply(Integer other) {
         // TODO implement to fix tests in FractionalTest4
-        return new Fractional(1, 1);
+        Fractional frac = new Fractional(numerator * other, denominator);
+        return frac;
     }
 
     public Fractional divide(Fractional other) {
@@ -48,7 +67,10 @@ public class Fractional {
 
     public Fractional divide(Integer other) {
         // TODO implement to fix tests in FractionalTest5
-        return new Fractional(1, 1);
+        //Fractional frac = new Fractional(numerator, denominator * other);
+        //return frac;
+        return new Fractional(this.numerator * 1, this.denominator * other);
+
     }
 
     public Fractional add(Fractional other) {
